26fc4c8dead82fb7186092f0f30ca93a240c4dee
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-06-17 19:51:11 -0300

    TERMINANDO OS TESTES UNITARIOS

--- Gerenciador_Passagens/teste-unitario/TesteUnitario.java
+++ Gerenciador_Passagens/teste-unitario/TesteUnitario.java
@@ -1,18 +1,27 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test ;
 
-import Modelo.Admin;
+import Modelo.*;
+import View.*;
+import Controle.*;
 
 public class TesteUnitario{
+	Memoria memoria = new Memoria();
+	TelaLogin telaLogin = new TelaLogin();
+	TelaLoginControle tlc = new TelaLoginControle(memoria, telaLogin);
+	Admin adm = new Admin(true, "teste","teste");
+	
 	@Test
 	void testNomeAdmin() {
-		Admin adm = new Admin(true, "teste","teste");
 		Assertions.assertEquals(adm.getNome(), "teste");
 	}
 	@Test
 	void testTipoAdmin() {
-		Admin adm = new Admin(true, "teste","teste");
 		Assertions.assertEquals(adm.getTipo(), true);
+	}
+	@Test 
+	void testAddAdmin(){
+		Assertions.assertEquals(tlc.verificacaoLogin("teste","teste", true), "erro");
 	}
 
 }
