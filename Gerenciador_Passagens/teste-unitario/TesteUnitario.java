b56e589c650299fd4aee4c889b5665e546bceaa1
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-06-20 21:29:26 -0300

    Adicionando o teste unitario

--- Gerenciador_Passagens/teste-unitario/TesteUnitario.java
+++ Gerenciador_Passagens/teste-unitario/TesteUnitario.java
@@ -1,26 +1,59 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test ;
+import org.junit.jupiter.api.Test;
 
-import Modelo.*;
-import View.*;
-import Controle.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import Controle.TelaAdminControle;
+import Controle.TelaLoginControle;
+import Modelo.Admin;
+import Modelo.Itinerario;
+import Modelo.Memoria;
+import View.TelaAdmin;
+import View.TelaLogin;
 
 public class TesteUnitario{
 	Memoria memoria = new Memoria();
 	TelaLogin telaLogin = new TelaLogin();
 	TelaLoginControle tlc = new TelaLoginControle(memoria, telaLogin);
 	Admin adm = new Admin(true, "teste","teste");
+	TelaAdmin ta = new TelaAdmin(adm);
+	TelaAdminControle tla = new TelaAdminControle(memoria, ta);
 	
 	@Test
-	void testNomeAdmin() {
-		Assertions.assertEquals(adm.getNome(), "teste");
+	void testAddItinerario() {
+		tla.addItinerario(LocalDate.of(2023, 12, 04), LocalDate.of(2023, 12, 05), 
+			  								   LocalTime.of(14, 0), LocalTime.of(18, 0),
+			  								   "Brasil", "Chile" );
+		Assertions.assertEquals(memoria.getListaItinerario().size(), 1);
 	}
 	@Test
-	void testTipoAdmin() {
-		Assertions.assertEquals(adm.getTipo(), true);
+	void testEditarItinerario() {
+		tla.addItinerario(LocalDate.of(2023, 12, 04), LocalDate.of(2023, 12, 05), 
+				   LocalTime.of(14, 0), LocalTime.of(18, 0),
+				   "Brasil", "Chile" );
+		
+		Itinerario it = memoria.getListaItinerario().get(0);
+		
+		tla.editItinerario(it.getIdItinerario(), 
+							LocalDate.of(2024, 12, 12), LocalDate.of(2024, 12, 13), 
+							LocalTime.of(11, 11), LocalTime.of(22, 22),
+							"TESTE", "TESTE2");
+		
+		Assertions.assertEquals(it.getDataInicial(),LocalDate.of(2024, 12, 12));
+		Assertions.assertEquals(it.getDataFinal(), LocalDate.of(2024, 12, 13));
+		Assertions.assertEquals(it.getHoraInicial(), LocalTime.of(11, 11));
+		Assertions.assertEquals(it.getHoraFinal(), LocalTime.of(22, 22));
+		Assertions.assertEquals(it.getPontPartida(),"TESTE");
+		Assertions.assertEquals(it.getPontChegada(), "TESTE2");
 	}
-	@Test 
-	void testAddAdmin(){
-		Assertions.assertEquals(tlc.verificacaoLogin("teste","teste", true), "erro");
+	@Test
+	void testDeleteItinerario() {
+		tla.addItinerario(LocalDate.of(2023, 12, 04), LocalDate.of(2023, 12, 05), 
+				   LocalTime.of(14, 0), LocalTime.of(18, 0),
+				   "Brasil", "Chile" );
+		tla.deleteItinerario(memoria.getListaItinerario().get(0).getIdItinerario());
+		
+		Assertions.assertEquals(memoria.getListaItinerario().size(), 0);
 	}
+
 }
